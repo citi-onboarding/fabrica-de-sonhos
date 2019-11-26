494c71f07058c41b33e2c831b0d45d75b03f8519
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2019-11-25 21:50:02 -0300

    Tive que refazer tudo do zero, mas pelo menos ta responsivo :-)

---
+++ static/Javascript/navbar.js
@@ -0,0 +1,23 @@
+// const header_navBar = document.querySelector('#header_navbar');
+// const navBar = header_navBar.querySelector('#navBar');
+
+// const navBar_desktop = navBar.querySelector('#navbar_svg');
+// const navBar_mobile = navBar.querySelector('navbar_svg_mobile');
+
+
+
+// const displayNavBarDesktop = () => {
+//     navBar_desktop.classList.remove('.display_none');
+// }
+
+// const doNotDisplayNavBarDesktop = () => {
+//     navBar_desktop.classList.add('.display_none');
+// }
+
+// const displayNavBarMobile = () => {
+//     navBar_mobile.classList.remove('.display_none');
+// }
+
+// const doNotDisplayNavBarMobile = () => {
+//     navBar_mobile.classList.add('.display_none');
+// }
