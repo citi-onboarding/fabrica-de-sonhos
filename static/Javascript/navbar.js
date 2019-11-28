c8f080d26c8075079cae5bdcadddc84735e58d63
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2019-11-28 18:07:54 -0300

    Navbar finalizada

--- static/Javascript/navbar.js
+++ static/Javascript/navbar.js
@@ -1,7 +1,9 @@
 const header_navBar = document.querySelector('#header_navbar');
 const button_menu_hamburguer = header_navBar.querySelector('.box_menu_hamburguer');
-const button_icon_exit = header_navBar.querySelector('.box_botao_sair');
 const action_div = header_navBar.querySelector('.action_menu_hamburguer');
+const button_icon_exit = action_div.querySelector('.box_botao_sair');
+const logo_mobile = action_div.querySelector('.logo_fabrica_mobile');
+const secoes_mobile = action_div.querySelector('.secoes_box_mobile');
 
 function menuHamburguerRemove() {
     button_menu_hamburguer.classList.add('remove_button');
@@ -27,6 +29,17 @@ function changeMenuScreenAdd() {
     action_div.classList.remove('remove_button');
 }
 
+$(window).resize(function() {
+    if(window.innerWidth <= 1024) {
+        button_menu_hamburguer.classList.remove('remove_button');
+        logo_mobile.classList.remove('remove_button');
+    }
+    else {
+        button_menu_hamburguer.classList.add('remove_button');
+        logo_mobile.classList.add('remove_button');
+    }
+})
+
 button_menu_hamburguer.addEventListener('click', () => {
     menuHamburguerRemove();
     iconExitAdd();
@@ -37,4 +50,9 @@ button_icon_exit.addEventListener('click', () => {
     iconExitRemove();
     menuHamburguerAdd();
     changeMenuScreenRemove();
+})
+
+secoes_mobile.addEventListener('click', () => {
+    changeMenuScreenRemove();
+    menuHamburguerAdd();
 })
