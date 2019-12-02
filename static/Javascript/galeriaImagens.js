391f8db46de7a49c17e467a2f310a2ae192016f3
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2019-12-02 16:53:54 -0300

    QUASE TUDO PRONTOOOOOO

--- static/Javascript/galeriaImagens.js
+++ static/Javascript/galeriaImagens.js
@@ -7,3 +7,35 @@ $(document).ready(function(){
         autoplaySpeed: 3000,
     });
 });
+
+// CORAÇÃO
+const galeria = document.querySelector('#galeria');
+const coracao_sem_mensagem = galeria.querySelector('#coracao_branco');
+const coracao_mensagem = galeria.querySelector('#coracao_mensagem');
+const mensagem = galeria.querySelector('.mensagem');
+
+function removeCoracaoSemMensagem() {
+    coracao_sem_mensagem.classList.add('remove_button');
+}
+
+function removeCoracaoComMensagem() {
+    coracao_mensagem.classList.add('remove_button');
+}
+
+function addCoracaoSemMensagem() {
+    coracao_sem_mensagem.classList.remove('remove_button');
+}
+
+function addCoracaoComMensagem() {
+    coracao_mensagem.classList.remove('remove_button');
+}
+
+coracao_sem_mensagem.addEventListener('click', () => {
+    removeCoracaoSemMensagem();
+    addCoracaoComMensagem();
+})
+
+coracao_mensagem.addEventListener('click', () => {
+    removeCoracaoComMensagem();
+    addCoracaoSemMensagem();
+})
