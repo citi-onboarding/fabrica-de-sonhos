19f1bda840511400c432abb917144a8f42ad3fe4
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2019-12-02 14:22:22 -0300

    detalhe na imagem

--- static/Javascript/galeriaImagens.js
+++ static/Javascript/galeriaImagens.js
@@ -1,6 +1,8 @@
+// CARROSSEL
 $(document).ready(function(){
     $('#carrossel').slick({
         dots: false,
+        arrows: false,
         autoplay: true,
         autoplaySpeed: 3000,
     });
