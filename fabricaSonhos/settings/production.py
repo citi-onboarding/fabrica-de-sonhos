9efc09d01b4ff02a33eb90ea660ec56dab66b6b1
Johnny-Herbert <jhmn@cin.ufpe.br>
2020-01-16 16:50:25 -0300

    Add settings https

---
+++ fabricaSonhos/settings/production.py
@@ -0,0 +1,7 @@
+from .base import *
+
+# SECURE_SSL_REDIRECT = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
+DROPBOX_OAUTH2_TOKEN = 'op_fyJe7oZAAAAAAAAAAJiC5_4oM98Wh-ujEM9KQblpcKVnuqZ-f4_LGhyFJFXqX'
