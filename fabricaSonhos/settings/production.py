7daa8f80bdead68ab2bb68978d939ec1369910ae
Johnny-Herbert <jhmn@cin.ufpe.br>
2020-01-16 17:12:31 -0300

    Add dotenv file

--- fabricaSonhos/settings/production.py
+++ fabricaSonhos/settings/production.py
@@ -4,4 +4,4 @@ from .base import *
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
-DROPBOX_OAUTH2_TOKEN = 'op_fyJe7oZAAAAAAAAAAJiC5_4oM98Wh-ujEM9KQblpcKVnuqZ-f4_LGhyFJFXqX'
+DROPBOX_OAUTH2_TOKEN = config('DROPBOX_TOKEN')
